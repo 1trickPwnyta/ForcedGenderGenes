90ec9201766996f79079dd43a21017b060dc42ef
1trickPwnyta <sarahpace37@gmail.com>
2024-09-18 18:33:30 -0700

    Complete mod

---
+++ 1.5/Source/FacialAnimationUtility.cs
@@ -0,0 +1,37 @@
+﻿using FacialAnimation;
+using HarmonyLib;
+using System.Linq;
+using Verse;
+
+namespace ForcedGenderGenes
+{
+    public static class FacialAnimationUtility
+    {
+        public static void UpdateGenderedFacialFeatures(this Pawn pawn)
+        {
+            EyeballControllerComp eyeComp = pawn.GetComp<EyeballControllerComp>();
+            if (eyeComp != null)
+            {
+                EyeballTypeDef def;
+                DefDatabase<EyeballTypeDef>.AllDefsListForReading.Where(d => d.gender == Gender.None || d.gender == pawn.gender).TryRandomElement(out def);
+                if (def != null)
+                {
+                    eyeComp.FaceType = def;
+                }
+            }
+
+            LidControllerComp lidComp = pawn.GetComp<LidControllerComp>();
+            if (lidComp != null)
+            {
+                LidTypeDef def;
+                DefDatabase<LidTypeDef>.AllDefsListForReading.Where(d => d.gender == Gender.None || d.gender == pawn.gender).TryRandomElement(out def);
+                if (def != null)
+                {
+                    lidComp.FaceType = def;
+                }
+            }
+
+            AccessTools.Field(typeof(NL_SelectPartWindow), "changed").SetValue(NL_SelectPartTabWindow.instance, true);
+        }
+    }
+}
---
+++ 1.5/Source/Gene_ForcedGender.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ForcedGenderGenes
+{
+    public class Gene_ForcedGender : Gene
+    {
+        private Gender originalGender;
+
+        public override void PostAdd()
+        {
+            base.PostAdd();
+            originalGender = pawn.gender;
+            if (def.defName == "FemaleOnly")
+            {
+                pawn.gender = Gender.Female;
+            }
+            else if (def.defName == "MaleOnly")
+            {
+                pawn.gender = Gender.Male;
+            }
+            Notify_GenderChanged();
+        }
+
+        public override void PostRemove()
+        {
+            base.PostRemove();
+            pawn.gender = originalGender;
+            Notify_GenderChanged();
+        }
+
+        public void Notify_GenderChanged()
+        {
+            if (pawn.story.bodyType == BodyTypeDefOf.Female || pawn.story.bodyType == BodyTypeDefOf.Male)
+            {
+                pawn.story.bodyType = GeneticBodyType.Standard.ToBodyType(pawn);
+            }
+
+            if (pawn.story.headType.gender != Gender.None && pawn.story.headType.gender != pawn.gender)
+            {
+                Verse.HeadTypeDef def = DefDatabase<Verse.HeadTypeDef>.GetNamedSilentFail(pawn.story.headType.defName.Replace(pawn.story.headType.gender.ToString(), pawn.gender.ToString()));
+                if (def != null)
+                {
+                    pawn.story.headType = def;
+                }
+            }
+            
+            if (pawn.gender == Gender.Female && !pawn.genes.HasActiveGene(DefDatabase<GeneDef>.GetNamed("Beard_Always")))
+            {
+                pawn.style.beardDef = BeardDefOf.NoBeard;
+            }
+
+            if (ModsConfig.IsActive("Nals.FacialAnimation"))
+            {
+                pawn.UpdateGenderedFacialFeatures();
+            }
+
+            pawn.Drawer.renderer.SetAllGraphicsDirty();
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref originalGender, "originalGender");
+        }
+    }
+}
